583389f75eefbd751e792faeb229a06e930fae83
wizardabhi77 <vemulapalliabhinav@gmail.com>
2025-12-01 21:06:20 +0530

    Add a lot of methods to LinkedList class
    prepend, at, contains,find, pop methods added

--- linkedList.js
+++ linkedList.js
@@ -22,6 +22,75 @@ class LinkedList {
         this.size++;
     }
 
+    prepend(value){
+        if(!this.head){
+            this.head = new Node();
+            this.head.value = value;
+            this.tail = this.head;
+        }
+        else {
+            let newNode = new Node();
+            newNode.value = value;
+            newNode.nextNode = this.head;
+            this.head = newNode;
+        }
+        this.size++;
+    }
+
+    at(index) {
+        if(index >= this.size) {
+            console.log('index Out of Bounds');
+        }
+        else {
+            let currNode = this.head;
+            for (let i=0; i <= this.size; i++){
+                if(i === index){
+                    console.log(`(${currNode.value}) is at INDEX:${index}`);
+                    break;
+                }
+                currNode = currNode.nextNode;
+            }
+        }
+    }
+
+    contains(value) {
+        let currNode = this.head;
+
+        for (let i=0; i < this.size; i++){
+            if(currNode.value === value){
+                return true;    
+            }
+            currNode = currNode.nextNode;
+        }
+
+        return false;
+    }
+
+    find(value) {
+        let currNode = this.head;
+
+        for (let i=0; i < this.size; i++){
+            if(currNode.value === value){
+                return i;    
+            }
+            currNode = currNode.nextNode;
+        }
+
+        return null;
+    }
+
+    pop(){
+        let currNode = this.head;
+
+        for (let i=0; i < this.size-1; i++){
+            currNode = currNode.nextNode;
+        }
+
+        this.tail = currNode;
+        this.tail.nextNode = null;
+        this.size--;
+    }
+
     toString() {
         
         let currNode = this.head;
--- main.js
+++ main.js
@@ -5,9 +5,22 @@ let list = new LinkedList();
 list.append('dog');
 list.append('cat');
 list.append('snake');
+list.prepend('lion');
+list.append('rabbit');
+
+list.pop();
+
 
 console.log(list.head);
 console.log(list.tail);
-console.log(list.size);
+console.log('SIZE:'+list.size);
 
 list.toString();
+
+list.at(3);
+
+console.log(list.contains('dog'));
+console.log(list.find('cat'));
+
+console.log(list.contains('dragon'));
+console.log(list.find('dragon'));
